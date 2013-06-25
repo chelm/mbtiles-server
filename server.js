df43810bb6c998356176a7ed77692884c103b462
tobinbradley <tobin.bradley@gmail.com>
2013-06-25 19:37:43 -0400

    changed gettile arg order to match mbtiles spec

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express"),
 
 if(process.argv.length < 3) {
   console.log("Error! Missing TILES filename.\nUsage: node server.js TILES");
-  process.exit(1)
+  process.exit(1);
 }
 
 var TILES = String(process.argv[2]).replace(/\.mbtiles/,'') + '.mbtiles';
@@ -13,12 +13,12 @@ new MBTiles(__dirname + '/' + TILES, function(err, mbtiles) {
   if (err) throw err;
   app.get('/:y/:x/:z.*', function(req, res) {
     // .getTile() expects XYZ.
-    console.log(req.param('y'), req.param('x'), req.param('z'))
-    mbtiles.getTile(req.param('y'), req.param('x'), req.param('z'), function(err, tile, headers) {
+    console.log(req.param('z'), req.param('x'), req.param('y'));
+    mbtiles.getTile(req.param('z'), req.param('x'), req.param('y'), function(err, tile, headers) {
       if (err) {
         res.send('Tile rendering error: ' + err + '\n');
       } else {
-        res.header("Content-Type", "image/png")
+        res.header("Content-Type", "image/png");
         res.send(tile);
       }
     });
